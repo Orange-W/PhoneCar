d12c5772e0d922446bdc21d7e7d4ccbcf75b560d
Orange-W <495871401@163.com>
2015-11-02 19:18:11 +0800

    Initial Commit

---
+++ 智能车/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  智能车
+//
+//  Created by user on 15/11/2.
+//  Copyright (c) 2015年 Orange-W. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 智能车/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  智能车
+//
+//  Created by user on 15/11/2.
+//  Copyright (c) 2015年 Orange-W. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
